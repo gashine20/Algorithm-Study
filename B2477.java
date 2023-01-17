fe16bf956ff78764f776ce3e808d5c855f9835a8
GaShine <100428958+Ga-Long@users.noreply.github.com>
2023-01-18 00:22:06 +0900

    기하1 - 참외밭 ㅠㅠ 틀렸대

---
+++ B2477.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class B2477 {
+    //기하1 - 참외밭
+    //(가로 max * 세로 max) - (가로 min * 세로 min)
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int K = Integer.parseInt(br.readLine()); //1m^2에 자라는 참외 수
+
+        int wmax, wmin, hmax, hmin;
+        int area = 0;
+        ArrayList<Integer> width = new ArrayList<Integer>();
+        ArrayList<Integer> height = new ArrayList<Integer>();
+
+        for (int i = 0; i < 6; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int direct = Integer.parseInt(st.nextToken()); //방향
+            // 1:동쪽, 2:서쪽, 3:남쪽, 4:북쪽
+            int len = Integer.parseInt(st.nextToken()); //최대 최소
+
+            if (direct == 1 || direct == 2) {
+                width.add(len);
+            } else if (direct == 3 || direct == 4) {
+                height.add(len);
+            }
+        }
+
+        //내림차순 - 최대
+        Collections.sort(width, Collections.reverseOrder());
+        wmax = width.get(0);
+        Collections.sort(height, Collections.reverseOrder());
+        hmax = height.get(0);
+
+        //오름차순 - 최소
+        Collections.sort(width);
+        wmin = width.get(0);
+        Collections.sort(height);
+        hmin = height.get(0);
+
+        area = ((wmax * hmax) - (wmin * hmin)) * K;
+        System.out.println(area);
+    }
+}
