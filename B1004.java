1fb14b1f2d6fc7c2bd599701084e7b9a96bcefa1
GaShine <100428958+Ga-Long@users.noreply.github.com>
2023-01-25 01:45:44 +0900

    기하1 - 어린왕자

--- B1004.java
+++ B1004.java
@@ -6,13 +6,14 @@ import java.util.StringTokenizer;
 public class B1004 {
     //기하1 - 어린왕자
     //점이 원 안에 있는가
+    //두 점 다 원 안에 있을 수 있음 - 제외!!
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine()); //테스트 케이스 개수
 
         int[] count = new int[T]; //진입/이탈 횟수
 
-        for(int i=0;i<T;i++){
+        for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
@@ -22,30 +23,27 @@ public class B1004 {
             int n = Integer.parseInt(br.readLine()); //행성계 개수
             int[][] planet = new int[n][3]; //진입/이탈 횟수
 
-            for(int j=0;j<n;j++){
+            for (int j = 0; j < n; j++) {
                 StringTokenizer st2 = new StringTokenizer(br.readLine());
                 planet[j][0] = Integer.parseInt(st2.nextToken());
                 planet[j][1] = Integer.parseInt(st2.nextToken());
                 planet[j][2] = Integer.parseInt(st2.nextToken());
             }
 
-            //점과 원의 중심의 거리 <= 반지름
-            PinC(count, i, x1, y1, n, planet);
-            PinC(count, i, x2, y2, n, planet);
+            for (int k = 0; k < n; k++) {
+                int dist1 = (x1 - planet[k][0]) * (x1 - planet[k][0]) + (y1 - planet[k][1]) * (y1 - planet[k][1]);//점과 원의 중심의 거리
+                int dist2 = (x2 - planet[k][0]) * (x2 - planet[k][0]) + (y2 - planet[k][1]) * (y2 - planet[k][1]);//점과 원의 중심의 거리
+                int r2 = (planet[k][2] * planet[k][2]);
+                if (!(dist1 <= r2 && dist2 <= r2)) {//두 점 다 원 안에 있을 수 있음 - 제외!!
+                    if (dist1 <= r2 || dist2 <= r2)
+                        count[i]++;
+                }
+            }
 
         }
-        for(int j:count)
+        for (int j : count)
             System.out.println(j);
 
-
     }
 
-    private static void PinC(int[] count, int i, int x2, int y2, int n, int[][] planet) {
-        for(int k=0;k<n;k++) {
-            int dist = (x2-planet[k][0])*(x2-planet[k][0]) + (y2-planet[k][1])*(y2-planet[k][1]);//점과 원의 중심의 거리
-            if(dist<=(planet[k][2]*planet[k][2])) {
-                count[i]++;
-            }
-        }
-    }
 }
