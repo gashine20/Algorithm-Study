0c676900d40118af98d33f31d8c1b2270d470271
GaShine <100428958+Ga-Long@users.noreply.github.com>
2023-02-19 23:32:55 +0900

    초콜릿 자르기

---
+++ B2163.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class B2163 {
+    //초콜릿 자르기
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        //2X2
+        System.out.println((N * M) - 1);
+    }
+}
