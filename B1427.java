c40bf0d0036ecaef675e3a7dde8f1b779feb4a8d
GaShine <100428958+Ga-Long@users.noreply.github.com>
2023-02-22 22:37:15 +0900

    소트인사이드

---
+++ B1427.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class B1427 {
+    //소트인사이드
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] num = new int[10];
+
+        int cnt = 0;
+        while (N != 0) {
+            num[cnt++] = N % 10;
+            N /= 10;
+        }
+
+        Arrays.sort(num);
+
+        for (int i = 9; i > 9 - cnt; i--) {
+            System.out.print(num[i]);
+        }
+    }
+}
