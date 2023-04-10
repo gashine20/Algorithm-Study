00064324c0b32b688959b296ddd7db1ad0817f23
gahyeon <1000ji1@naver.com>
2023-04-10 22:29:33 +0900

    문자와 문자열

---
+++ B2941.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class B2941 {
+    //크로아티아 알파벳
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> cro = new ArrayList<>();
+        cro.add("c=");cro.add("c-");cro.add("dz=");cro.add("d-");cro.add("li");
+        cro.add("nj");cro.add("s=");cro.add("z=");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        
+    }
+}
