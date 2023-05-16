86391314dc06ab4fc49b1fd0d20738b6137b2c3e
gahyeon <1000ji1@naver.com>
2023-05-16 23:38:23 +0900

    최댓값

--- Python/B2566.py
+++ Python/B2566.py
@@ -1,15 +1,20 @@
 # 최댓값
 
-table = [list(map(int, input().split())) for _ in range(9)]
+num = [[0]*9]*9
 
-max_num = 0
-max_row, max_col = 0, 0
-for row in range(9):
-    for col in range(9):
-        if max_num <= table[row][col]:
-            max_row = row + 1
-            max_col = col + 1
-            max_num = table[row][col]
+for i in range(9):
+    num[i] = list(map(int,input().split()))
 
-print(max_num)
-print(max_row, max_col)
+max = num[0][0]
+row = 0
+col = 0
+#print(max)
+for i in range(9):
+    for j in range(9):
+        if(num[i][j]>max):
+            max = num[i][j]
+            row = i
+            col = j
+
+print(max)
+print(row+1,col+1)
