b24307f1b632978db6a84454dd0c93d106aa8711
gahyeon <1000ji1@naver.com>
2023-07-03 13:52:53 +0900

    수 정렬하기

--- Python/B2750.py
+++ Python/B2750.py
@@ -1,9 +1,12 @@
 # 수 정렬하기
 
-x = int(input())
-num_list = []
-for i in range(x):
-    num_list.append(int(input()))
-num_list1 = sorted(num_list)
-for i in range(len(num_list)):
-    print(num_list1[i])
+N = int(input())
+
+a = []
+for _ in range(N):
+    b = int(input())
+    a.append(b)
+
+a.sort()
+for i in a:
+    print(i)
