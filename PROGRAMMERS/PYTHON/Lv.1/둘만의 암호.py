52ab1205e17601f5e26e14056e89145fad537173
gahyeon <1000ji1@naver.com>
2023-08-22 00:01:21 +0900

    둘만의 암호

--- PROGRAMMERS/PYTHON/Lv.1/둘만의 암호.py
+++ PROGRAMMERS/PYTHON/Lv.1/둘만의 암호.py
@@ -1,22 +1,15 @@
 def solution(s, skip, index):
     answer = ''
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+
+    for i in skip:
+        alpha = alpha.replace(i, "")
 
     for i in s:  # "aukks"
-        next = ord(i)
-        count = 1
-        idx = 0
+        next = alpha.index(i) + index
+        if next >= len(alpha):
+            next %= len(alpha)
 
-        while (count <= index):
-            # print(idx)
-            idx += 1
-            if chr(next + idx) in skip:  # skip에 포함되어 있으면
-                pass
-            else:  # 포함 안되어 있으면
-                if next + idx >= 122:  # z 넘고
-                    idx -= 26
-                count += 1
-
-        next = next + idx
-        answer += chr(next)
+        answer += alpha[next]
 
     return answer
