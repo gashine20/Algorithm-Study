00d9b89145a7440fc5d2fdd7fd8b0613f8b3dbbd
GaShine <100428958+Ga-Long@users.noreply.github.com>
2023-09-28 21:45:41 +0900

    [Bronze I] Title: 평균, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.py
@@ -0,0 +1,12 @@
+# 평균
+N = int(input())
+
+scores = list(map(int, input().split()))
+
+max_score = max(scores)
+
+sum_score = 0
+for score in scores:
+    sum_score += (score / max_score) * 100
+
+print(sum_score / N)
