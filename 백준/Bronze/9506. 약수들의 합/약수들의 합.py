798b02406d89616695f50eaa63284044e78241d2
GaShine <100428958+Ga-Long@users.noreply.github.com>
2023-11-06 21:47:29 +0900

    [Bronze I] Title: 약수들의 합, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -0,0 +1,19 @@
+# 약수들의 합
+
+while True:
+    a = int(input())
+    if (a == -1): break
+    
+    num = []
+    for i in range(1,a):
+        if(a % i ==0):
+            num.append(i)
+
+    sum = 0
+    for i in num:
+        sum += i
+    if(sum == a):
+        print(a, "=", end =" ")
+        print(*num, sep=" + ")
+    else:
+        print(a,"is NOT perfect.")
