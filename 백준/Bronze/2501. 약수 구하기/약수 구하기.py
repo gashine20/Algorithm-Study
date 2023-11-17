977fa6c653c9e6f4d1488becdbac9e5837f19564
GaShine <100428958+Ga-Long@users.noreply.github.com>
2023-11-17 15:04:15 +0900

    [Bronze III] Title: 약수 구하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.py
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -1,15 +1,12 @@
-# 약수 구하기
-
 N, K = map(int, input().split())
 
-index = 0
-number = []
-for i in range(1, N + 1):
-    if (N % i == 0):
-        number.append(i)
-        index = index + 1
+divisor = []
 
-if len(number) < K:
+for i in range(1, N + 1):
+    if N % i == 0:
+        divisor.append(i)
+
+if len(divisor) < K:
     print("0")
 else:
-    print(number.pop(K - 1))
+    print(divisor[K - 1])
