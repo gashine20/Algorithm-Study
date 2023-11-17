7e6ae1960c6cae0839656fc7e6359d219c25d3ab
GaShine <100428958+Ga-Long@users.noreply.github.com>
2023-11-17 14:33:04 +0900

    [Bronze II] Title: 소수, Time: 196 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,26 @@
+# 소수
+
+def PrimeNum(a):
+    if a == 1 : return 0
+    for i in range(2,int(a/2)+1):
+        if(a % i == 0): #약수
+            return 0
+    
+    else: return 1
+            
+M = int(input())
+N = int(input())
+
+PrimeNumber = []
+for i in range(M, N+1):
+    if PrimeNum(i) == 1: 
+        PrimeNumber.append(i)
+
+if len(PrimeNumber) == 0: print("-1")
+else:
+    sum = 0
+    for i in PrimeNumber:
+        sum += i
+
+    print(sum)
+    print(PrimeNumber[0])
