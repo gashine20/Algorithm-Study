668fd4af902b7d4d271014d3836084e61ae57538
GaShine <100428958+Ga-Long@users.noreply.github.com>
2023-11-24 01:48:49 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,39 +1,60 @@
 def solution(s):
     answer = []
-    s = s[1:len(s)-1]
-    s_list = list(s)
+    s2 = s.lstrip('{').rstrip('}').split('},{') # 감탄..
     
-    #print(s_list)
+    s3 = []
+    for element in s2:
+        s3.append(list(map(int,element.split(','))))
     
-    # string -> array
-    s1 = []
-    s_array = []
-    operand = ''
-    for element in s_list:
-        if element == '{':
-            s_array = [] # 초기화
-            operand = ''
-        elif element == '}':
-            s_array.append(int(operand))
-            s1.append(s_array)
-            operand = '' # 초기화
-        elif element == ',':
-            if operand != '':
-                s_array.append(int(operand))
-                operand = '' # 초기화
-        else: # 피연산자일 때
-            operand += element
+    s3.sort(key=len)
     
-    #print(s1)
-    
-    s1.sort(key=len)
-    #print(s1)
-    
-    # 순서대로 넣기
-    for collection in s1:
-        #print(collection)
+    for collection in s3:
         for element in collection:
             if element not in answer:
                 answer.append(element)
     
     return answer
+
+
+
+
+# 처음 풀은 방식
+# def solution(s):
+#     answer = []
+#     s = s[1:len(s)-1]
+#     s_list = list(s)
+    
+#     #print(s_list)
+    
+#     # string -> array
+#     s1 = []
+#     s_array = []
+#     operand = ''
+#     for element in s_list:
+#         if element == '{':
+#             s_array = [] # 초기화
+#             operand = ''
+#         elif element == '}':
+#             s_array.append(int(operand))
+#             s1.append(s_array)
+#             operand = '' # 초기화
+#         elif element == ',':
+#             if operand != '':
+#                 s_array.append(int(operand))
+#                 operand = '' # 초기화
+#         else: # 피연산자일 때
+#             operand += element
+    
+#     #print(s1)
+    
+#     s1.sort(key=len)
+#     #print(s1)
+    
+#     # 순서대로 넣기
+#     for collection in s1:
+#         #print(collection)
+#         for element in collection:
+#             if element not in answer:
+#                 answer.append(element)
+    
+#     return answer
