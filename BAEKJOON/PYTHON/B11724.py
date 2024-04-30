1dd8dfc2a481841f2a1678f9b0d84f516281953e
gahyeon <1000ji1@naver.com>
2024-04-30 23:43:37 +0900

    연결 요소의 개수

---
+++ BAEKJOON/PYTHON/B11724.py
@@ -0,0 +1,32 @@
+# 11724 연결 요소의 개수
+import sys
+
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+node = [[] for _ in range(N + 1)]
+visited = [False] * (N + 1)
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    node[u].append(v)
+    node[v].append(u)
+
+
+# dfs - 깊이 탐색
+def dfs(start):
+    visited[start] = True
+    for next in node[start]:
+        if not visited[next]:
+            dfs(next)
+
+
+answer = 0
+for i in range(1, N + 1):
+    if not visited[i]:
+        dfs(i)
+        answer += 1
+
+print(answer)
