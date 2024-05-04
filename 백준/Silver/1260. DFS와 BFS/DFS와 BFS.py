0cb03d7d72ac14e539f2ea70d94d15511dc078ef
GaShine <100428958+Ga-Long@users.noreply.github.com>
2024-05-05 02:58:26 +0900

    [Silver II] Title: DFS와 BFS, Time: 472 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,55 +1,48 @@
-# DFS: 한 방향으로 갈 수 있을때까지 계속 가다가, 더이상 길이 없으면 가장 짧은 거리로 다시 돌아가 다른 방향으로 이동!
-# 예시 추가
-# 6 5 1
-# 1 2
-# 2 3
-# 3 4
-# 2 5
-# 5 6
+# DFS와 BFS
+# 깊이 탐색 - 재귀, 너비 탐색 - 큐, 선입선출
+
 from collections import deque
+import sys
+
+sys.setrecursionlimit(10000)
 
 N, M, V = map(int, input().split())
-# A 그래프 데이터 저장 인접 리스트
-A = [[] for _ in range(N + 1)]
-visited = []
+
+node = [[] for _ in range(N + 1)]
+visited = [False] * (N + 1)
+myque = deque()
 
 for _ in range(M):
     s, e = map(int, input().split())
-    A[s].append(e)
-    A[e].append(s)
+    node[s].append(e)
+    node[e].append(s)
 
-# 정렬
-for i in range(1, len(A)):
-    A[i].sort()
+    node[s].sort()
+    node[e].sort()
+
+def dfs(start):
+    print(start, end=" ")
+    visited[start] = True
+
+    for next in node[start]:
+        if not visited[next]:
+            dfs(next)
+
+def bfs(start):
+    visited[start] = True
+    myque.append(start)
+
+    while myque:
+        now = myque.popleft()
+        print(now, end=" ")
+
+        for next in node[now]:
+            if not visited[next]:
+                visited[next] = True
+                myque.append(next)
 
 
-def DFS(v):  # 인자: 시작 edge
-    print(v, end=" ")
-    visited.append(v)
-    for node in A[v]:
-        if node not in visited:
-            DFS(node)
-
-
-DFS(V)
-print()
-
-# BFS : 너비 우선 탐색,
-# : 루트노드에서 인접한 노드 먼저 탐색, 재귀적x, 큐FIFO, 더이상 방문할 곳 없으면 탐색마침
+dfs(V)
 visited = [False] * (N + 1)
-
-
-def BFS(v):
-    queue = deque()
-    queue.append(v)
-    visited[v] = True
-    while queue:
-        now_Node = queue.popleft()
-        print(now_Node, end=" ")
-        for node in A[now_Node]:
-            if not visited[node]:
-                visited[node] = True
-                queue.append(node)
-
-
-BFS(V)
+print()
+bfs(V)
