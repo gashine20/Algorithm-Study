b63a7870bb6d643e5bea344378bb554815e911dc
GaShine <100428958+Ga-Long@users.noreply.github.com>
2024-05-20 15:10:27 +0900

    [Gold V] Title: 집합의 표현, Time: 300 ms, Memory: 71112 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,42 +1,36 @@
-# 유니온 파인드 알고리즘 (Union Find)
-# 어떤 두 개의 노드를 같은 집합으로 묶어주고, 어떤 두 노드가 같은 집합에 있는지 확인하는 알고리즘
+# 집합의 표현
 import sys
 
 input = sys.stdin.readline
-sys.setrecursionlimit(1000000)
+sys.setrecursionlimit(10000)
 
 n, m = map(int, input().split())
-arr = [i for i in range(n + 1)]
+A = [i for i in range(n + 1)]
 
 
-def find_index(number):
-    if arr[number] != number:
-        arr[number] = find_index(arr[number])
-    return arr[number]
+# def findParent(num):
+#     if A[num] == num:
+#         return num
+#     else:
+#         return findParent(A[num])
+
+def findParent(num):
+    if A[num] != num:
+        A[num] = findParent(A[num])
+    return A[num]
 
 
-def arr_union(a, b):
-    a_index = find_index(a)
-    b_index = find_index(b)
 
-    if a_index != b_index:
-        arr[b_index] = a_index
+for i in range(m):
+    kind, a, b = map(int, input().split())
+    parent1 = findParent(a)
+    parent2 = findParent(b)
 
-
-def arr_find(a, b):
-    a_index = find_index(a)
-    b_index = find_index(b)
-
-    if a_index == b_index:
-        return "YES"
-    else:
-        return "NO"
-
-
-for _ in range(m):
-    question, a, b = map(int, input().split())
-    if question == 0:
-        arr_union(a, b)
-        # print(arr)
-    elif question == 1:
-        print(arr_find(a, b))
+    if kind == 0:  # a, b 합친다
+        if parent1 != parent2:
+            A[parent2] = parent1
+    elif kind == 1:
+        if parent1 == parent2:
+            print("YES")
+        else:
+            print("NO")
