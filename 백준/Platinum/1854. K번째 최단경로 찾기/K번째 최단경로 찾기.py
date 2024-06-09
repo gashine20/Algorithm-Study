510413c0a21dee0b59f987c86c45687c54389c3e
GaShine <100428958+Ga-Long@users.noreply.github.com>
2024-06-09 16:10:29 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 2960 ms, Memory: 66672 KB -BaekjoonHub

--- 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.py
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.py
@@ -1,39 +1,42 @@
-# 다익스트라
+# K번째 최단경로 찾기
 import sys
-import heapq  # 우선순위 큐
+from queue import PriorityQueue
 
 input = sys.stdin.readline
 
 n, m, k = map(int, input().split())
 
-direction = [[] for _ in range(n + 1)]
-distance = [[sys.maxsize] * k for _ in range(n + 1)]
+graph = [[] for _ in range(n + 1)]
+distance = [[sys.maxsize for _ in range(k)] for _ in range(n + 1)]
+myqueue = PriorityQueue()
 
 for _ in range(m):
-    start, end, time = map(int, input().split())
-
-    direction[start].append([end, time])
-
-# print(direction)
-distance[1][0] = 0
-#print(distance)
-pq = [(0, 1)]
-
-while pq:
-    cost, node = heapq.heappop(pq)
-
-    for nextNode, nextCost in direction[node]:
-        sumCost = cost + nextCost
-
-        if distance[nextNode][k - 1] > sumCost:
-            distance[nextNode][k - 1] = sumCost
-            distance[nextNode].sort()
-
-            heapq.heappush(pq, [sumCost, nextNode])
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
 
 
-for dis in distance[1:]:
-    if dis[k-1] == sys.maxsize:
+def dijkstra(start):
+    distance[start][0] = 0
+    myqueue.put((0, start))
+
+    while myqueue.qsize() > 0:
+        now = myqueue.get()
+        now_node = now[1]
+        now_cost = now[0]
+
+        for next, weight in graph[now_node]:
+            total_distance = now_cost + weight
+            if distance[next][k - 1] > total_distance:  # 계속 갱신이 된다. (6,7)일 때 6이 들어오면 (6,6)으로
+                distance[next][k - 1] = total_distance
+                myqueue.put((total_distance, next))
+                distance[next].sort()  # 정렬
+            # print(distance)
+
+
+dijkstra(1)
+
+for i in range(1, n + 1):
+    if distance[i][k - 1] == sys.maxsize:
         print(-1)
     else:
-        print(dis[k-1])
+        print(distance[i][k - 1])
