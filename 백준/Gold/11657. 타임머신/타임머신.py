ab0540b6d69415ec04e6e3a8be562a4ff3156800
GaShine <100428958+Ga-Long@users.noreply.github.com>
2024-06-11 23:43:06 +0900

    [Gold IV] Title: 타임머신, Time: 1064 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.py
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -1,103 +1,38 @@
-# 벨만 포드 알고리즘
-
+# 타임머신
+# 벨만-포드 음수 가중치가 있는 경우
 import sys
 
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-
-# direction = [[] for _ in range(N + 1)]
-distance = [sys.maxsize] * (N + 1)
-# myque = deque()
-edges = []
-
+graph = []
+result = [sys.maxsize] * (N + 1)
 
 for _ in range(M):
-    start, end, time = map(int, input().split())
-    edges.append([start, end, time])
+    A, B, C = map(int, input().split())
+    graph.append((A, B, C))
 
+result[1] = 0
 
-distance[1] = 0
+for _ in range(N - 1):
 
-for _ in range(N-1):
+    for start, end, time in graph:  # result[start]!=sys.maxsize 조건이 있어야 1에서 출발할 수 있는 노선이 있는지 확인 가능하다.
+        if result[start] != sys.maxsize and result[end] > result[start] + time:
+            result[end] = result[start] + time
 
-    for start, end, time in edges:
-        if distance[start] != sys.maxsize and distance[start] + time < distance[end]:
-            distance[end] = distance[start] + time
+# 음수 사이클 존재 여부 확인
+# 에지 개수만큼 반복
 
-myCycle = False
+mCycle = False
+for start, end, time in graph:
+    if result[start] != sys.maxsize and result[end] > result[start] + time:
+        mCycle = True
 
-for start, end, time in edges:
-    if distance[start] != sys.maxsize and distance[start] + time < distance[end]:
-        myCycle = True
-
-if myCycle:
-    print(-1)
-else:
-    for dis in distance[2:]:
-        if dis == sys.maxsize:
-            print(-1)
+if not mCycle:
+    for i in range(2, N + 1):
+        if result[i] != sys.maxsize:
+            print(result[i])
         else:
-            print(dis)
-
-
-# 입력
-# for _ in range(M):
-#     start, end, time = map(int, input().split())
-#
-#     found = False
-#
-#     for info in direction[start]:
-#         if info[0] == end:
-#             if info[1] > time:  # 더 짧은 시간이 있으면 그걸로
-#                 found = True
-#                 info[1] = time
-#                 break
-#
-#     if not found:
-#         direction[start].append([end, time])
-
-# print(direction)
-
-# 내풀이 - 틀림..
-# for _ in range(N-1):
-#     # 초기 설정
-#     myque.append(1)
-#
-#     visited = [False for _ in range(N + 1)]
-#
-#     while myque:
-#         now = myque.popleft()
-#
-#         if visited[now]:
-#             continue
-#
-#         visited[now] = True
-#
-#         for info in direction[now]:
-#             next = info[0]
-#             time = info[1]
-#
-#             if distance[now] + time < distance[next]:
-#                 distance[next] = distance[now] + time
-#                 myque.append(next)
-#
-#             # print(distance)
-
-# def contain_negative(distance):
-#     for dis in distance[2:]:
-#         if dis < 0:
-#             return True
-#
-#     return False
-#
-#
-# if contain_negative(distance):
-#     print(-1)
-# else:
-#     for dis in distance[2:]:
-#         if dis == sys.maxsize:
-#             print(-1)
-#         else:
-#             print(dis)
-
+            print(-1)
+else:
+    print(-1)
