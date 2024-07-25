b7abce97573e9548ff70264316f90aa4d7c6cc93
GaShine <100428958+Ga-Long@users.noreply.github.com>
2024-07-25 23:16:15 +0900

    [Silver III] Title: 조약돌 꺼내기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.py
+++ 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.py
@@ -13,38 +13,16 @@ pickChance = 0
 for c in color:
     total += c
 
+ans = 0
+probability = [1] * 51
+
 for i in range(M):
-    C = color[i]
-    if C < K:
+    if color[i] < K:
         continue
 
-    T = [[0 for _ in range(C + 1)] for _ in range(C + 1)]
-    for i in range(C + 1):
-        T[i][0] = 1
-        T[i][i] = 1
-        T[i][1] = i
+    for k in range(K):
+        probability[i] = probability[i] * (color[i] - k) / (total - k)
 
-    for i in range(1, C + 1):
-        for j in range(1, C + 1):
-            T[i][j] = T[i - 1][j] + T[i - 1][j - 1]
+    ans += probability[i]
 
-    # for t in T:
-    #     print(t)
-
-    pickChance += T[C][K]
-
-# 전체 경우의 수
-T = [[0 for _ in range(total + 1)] for _ in range(total + 1)]
-for i in range(total + 1):
-    T[i][0] = 1
-    T[i][i] = 1
-    T[i][1] = i
-
-for i in range(1, total + 1):
-    for j in range(1, total + 1):
-        T[i][j] = T[i - 1][j] + T[i - 1][j - 1]
-
-totalChance = T[total][K]
-
-result = pickChance / totalChance
-print(result)
+print(ans)
