0f9bd44f4977c37c7339e668472205bc88453b89
gahyeon <1000ji1@naver.com>
2024-08-31 23:58:20 +0900

    연속 합2

---
+++ BAEKJOON/PYTHON/B13398.py
@@ -0,0 +1,25 @@
+# 연속합2
+
+n = int(input())
+A = list(map(int, input().split()))
+L = [0] * n  # 0~i 번째까지의 최대 합
+R = [0] * n  # n~i 번째까지의 최대 합
+
+
+L[0] = A[0]
+result = L[0]
+
+R[n - 1] = A[n - 1]
+
+for i in range(1, n):
+    L[i] = max(A[i], L[i - 1] + A[i])
+    result = max(result, L[i])
+
+for i in range(n - 2, -1, -1):
+    R[i] = max(A[i], R[i + 1] + A[i])
+
+for i in range(1, n - 1):
+    temp = L[i - 1] + R[i + 1]  # 0 ~ i+1 구간에서 i 뺀
+    result = max(result, temp)
+
+print(result)
