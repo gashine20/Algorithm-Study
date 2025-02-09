6d39c88a1e2fa40a41e20c888b85350ba1b71c9e
GaShine <100428958+Ga-Long@users.noreply.github.com>
2025-02-09 13:36:38 +0900

    [Gold V] Title: 퇴사 2, Time: 2260 ms, Memory: 169028 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,23 +1,27 @@
-# dp 의미를 뭐로 두어야 할지 모르겠다..
+# 퇴사2
 import sys
 
 input = sys.stdin.readline
 
 n = int(input())
-schedule = [] * (n + 1)
+T = [] * (n + 1)
+P = [] * (n + 1)
+D = [0 for _ in range(n + 1)]
 
-schedule.append([0, 0])
-for _ in range(n):
-    schedule.append(list(map(int, input().split())))
-
-# print(schedule)
-dp = [0 for _ in range(n + 1)]
+T.append(0)
+P.append(0)
 
 for i in range(1, n + 1):
-    dp[i] = max(dp[i], dp[i - 1])  # 이전까지의 최댓값
-    fin_date = i + schedule[i][0] - 1
-    if fin_date <= n:  # 최종일 안에 일이 끈나는 경우
-        # i일부터는 일을 해야하므로 i일에 얻을 수 있는 최댓값이 아닌 i-1일까지 얻을 수 있는 최댓값을 구한다...
-        dp[fin_date] = max(dp[fin_date], dp[i - 1] + schedule[i][1])
-    # print(fin_date, dp)
-print(max(dp))
+    t, p = map(int, input().split())
+    T.append(t)
+    P.append(p)
+
+result = 0
+
+for i in range(1, n + 1):
+    D[i] = max(D[i], D[i - 1])
+    pos = i + T[i] - 1
+    if pos <= n:
+        D[pos] = max(D[pos], D[i - 1] + P[i])
+
+print(max(D))
