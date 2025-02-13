2a985e282aafb8b3f555ff6feede4b222ed8af10
GaShine <100428958+Ga-Long@users.noreply.github.com>
2025-02-13 23:13:39 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 560 ms, Memory: 65156 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,30 +1,30 @@
-# 11724 연결 요소의 개
 import sys
-
 sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
+n, m = map(int, input().split())
+A = [[] for _ in range(n+1)]
+visited = [False] * (n + 1)
 
-node = [[] for _ in range(N + 1)]
-visited = [False] * (N + 1)
-
-for _ in range(M):
-    u, v = map(int, input().split())
-    node[u].append(v)
-    node[v].append(u)
-
-
-# dfs - 깊이 탐색
-def dfs(start):
-    visited[start] = True
-    for next in node[start]:
-        if not visited[next]:
-            dfs(next)
-
+for _ in range(m):
+    s, e = map(int, input().split())
+    A[s].append(e)
+    A[e].append(s)
 
 answer = 0
-for i in range(1, N + 1):
+
+
+def dfs(index):  # 깊이탐색 - visited를 처리하는
+    visited[index] = True
+
+    for a in A[index]:
+        if not visited[a]:
+            dfs(a)
+
+        #print(visited)
+
+
+for i in range(1, n + 1):
     if not visited[i]:
         dfs(i)
         answer += 1
