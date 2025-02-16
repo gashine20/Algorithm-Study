2403c9a8ba916aecd5dc5e2604fd2ec5404de851
GaShine <100428958+Ga-Long@users.noreply.github.com>
2025-02-16 23:37:19 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,28 +1,17 @@
 # 일곱 난쟁이
-import sys
 
-input = sys.stdin.readline
-height = []
+from itertools import combinations
+
+heights = [] * 9
 for _ in range(9):
-    height.append(int(input()))
+    heights.append(int(input()))
 
-height.sort()
-total_sum = sum(height)
-want_height = total_sum - 100
-
-
-def printHeight(i, j):
-    for k in range(len(height)):
-        if k != i and k != j:
-            print(height[k])
-
-
-check = False
-for i in range(8):
-    for j in range(i + 1, 9):
-        height_sum = height[i] + height[j]
-        if height_sum == want_height:
-            printHeight(i, j)
-            check = True
-    if check:
+result = []
+for height in list(combinations(heights, 7)):
+    if sum(height) == 100:
+        result = height
         break
+
+result = sorted(result)
+for i in result:
+    print(i)
