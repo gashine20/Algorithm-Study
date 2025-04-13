85b314225b49492b02ad35ae992206398283265b
Gahyeon Lee <100428958+gashine20@users.noreply.github.com>
2025-04-13 12:40:39 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.05 ms, Memory: 14.8 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -2,18 +2,9 @@ import sys
 
 def solution(arr):
     answer = []    
-    min_num = sys.maxsize
-    index = 0   
-
-    for i in range(len(arr)):
-        if arr[i] < min_num:
-            min_num = arr[i]
-            index = i
+    min_num = min(arr)
+    arr.remove(min_num)
     
-    arr.pop(index)
-    if len(arr) == 0:
-        answer = [-1]
-    else:
-        answer = arr
+    answer = [-1] if len(arr) == 0 else arr
     
     return answer
