13603bda0c89facd3493b165cc0e65908a4b069e
Gahyeon Lee <100428958+gashine20@users.noreply.github.com>
2025-04-25 02:36:04 +0900

    [level 2] Title: 주식가격, Time: 97.62 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,16 +1,35 @@
-class Solution{
-	public int[] solution(int[] prices) {
-		int len = prices.length;
-		int[] answer = new int[len];
-		
-		for(int i = 0; i < len; i++){
-			for(int j = i+1; j < len; j++){
-				answer[i]++;
-				if(prices[i]>prices[j]){
-					break;
-				}
-			}
-		}
-		return answer;	
-	}
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Queue<int[]> queue = new LinkedList<>();
+        int len = prices.length;
+        
+        // (1,1),(2,2),(3,3),(4,2),(5,3),(5,0)
+        for(int i = 0; i < len; i++){
+            int[] array = new int[2];
+            array[0] = i + 1;
+            array[1] = prices[i];
+            queue.add(array);
+        }
+        int[] array = new int[2];
+        array[0] = len;
+        array[1] = 0;
+        queue.add(array);
+        
+        while(!queue.isEmpty()){
+            int[] now = queue.poll();
+            
+            for(int[] next: queue){
+                if(now[1] > next[1]){
+                    result.add(next[0]-now[0]);
+                    break;
+                }
+            }
+        }
+        
+        int[] answer = result.stream().mapToInt(i -> i).toArray();
+        return answer;
+    }
 }
