7b6f701a019a8e1a55eef32660d503b2c76ce16d
Gahyeon Lee <100428958+gashine20@users.noreply.github.com>
2025-07-07 19:47:23 +0900

    [level 2] Title: 피로도, Time: 2.72 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,57 +1,26 @@
 import java.util.*;
 
 class Solution {
-    public int countPlay(int[] order, int[][] dungeons, int k){
-        int play = 0;
-        for(int index: order){
-            if(k >= dungeons[index][0]){
-                play++;
-                k-= dungeons[index][1];
-            }
-        }
-        return play;
-    }
-    
-    public void permute(int[] nums, int start, List<int[]> orders){
-        if(start == nums.length){
-            orders.add(nums.clone());
-            return;
-        }
-        
-        for(int i = start; i < nums.length; i++){
-            swap(nums, start, i);
-            permute(nums, start +1, orders);
-            swap(nums, start, i); // 백트래킹
-        }
-    }
-    
-    public void swap(int[] arr, int i, int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+    int answer = -1;
     
     public int solution(int k, int[][] dungeons) {
-        // 순열 만듦
-        int[] nums = new int[dungeons.length];
-        for(int i = 0; i < dungeons.length; i ++){
-            nums[i] = i;
-        }
+
+        boolean[] visited = new boolean[dungeons.length];
+        int cnt = 0;
         
-        List<int[]> orders = new ArrayList<>();
-        
-        permute(nums, 0, orders);
-        
-        
-        // play한 결과 담기
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int[] order: orders){
-            int play = countPlay(order, dungeons, k);
-            list.add(play);
-        }
-        
-        int answer = Collections.max(list);
+        dfs(k, dungeons, visited, cnt);
         
         return answer;
+    }
+    
+    public void dfs(int k, int[][] dungeons, boolean[] visited, int cnt){
+        for(int i = 0; i < dungeons.length; i++){
+            if(!visited[i] && k >= dungeons[i][0]){
+                visited[i] = true;
+                dfs(k-dungeons[i][1], dungeons, visited, cnt+1);
+                visited[i] = false;
+            }
+        }
+        answer = Math.max(answer, cnt);
     }
 }
