7b3f365e3080b579b079a160a95eff79ac6df0da
Gahyeon Lee <100428958+gashine20@users.noreply.github.com>
2025-07-26 22:59:26 +0900

    [level 2] Title: 더 맵게, Time: 1917.31 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,21 +1,21 @@
-from queue import PriorityQueue
+import heapq as hq
 
 def solution(scoville, K):
     answer = 0
-    pq = PriorityQueue()
-    for s in scoville:
-        pq.put(s)
     
+    hq.heapify(scoville)
+
     while True:
-        first = pq.get()
+        first = hq.heappop(scoville)
+        
         if first >= K:
             break
-        if first < K and pq.qsize() <= 0:
+        if first < K and len(scoville) <= 0:
             answer = -1
             break
             
-        second = pq.get()
-        pq.put(first + (second * 2))
+        second = hq.heappop(scoville)
+        hq.heappush(scoville, first + (second * 2))
         answer+=1
     
     return answer
