338a847e67f1056d0b6d9604b6d8178205577b5d
Gahyeon Lee <100428958+gashine20@users.noreply.github.com>
2025-08-06 20:56:56 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.87 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
 
 ### 성능 요약
 
-메모리: 74.3 MB, 시간: 17.56 ms
+메모리: 9.3 MB, 시간: 6.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 08일 17:50:28
+2025년 08월 06일 20:56:51
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,51 +1,42 @@
 from collections import deque
-import sys
 
-def bfs(start, visited, graph):
-    queue = deque()
+def bfs(visited, graph, start):
+    count = 1
+    q = deque()
+    q.append(start)
     visited[start] = True
-    queue.append(start)
-
-    count = 0
-    while queue:
-        now = queue.popleft()
-        count += 1
-        for next in graph[now]:
-            if not visited[next]:
-                visited[next] = True
-                queue.append(next)
+    
+    while q:
+        now_node = q.popleft()
+        
+        for next_node in graph[now_node]:
+            if not visited[next_node]:
+                visited[next_node] = True
+                q.append(next_node)
+                count += 1
+                
     return count
 
 
 def solution(n, wires):
-    answer = 100
-    visited = [False] * (n+1)
-    graph = [[] for _ in range(n+1)]
+    answer = 101
     
-    for wire in wires: # graph 초기화
-        s = wire[0]
-        e = wire[1]
-        graph[s].append(e)
-        graph[e].append(s)
-    
-    for wire in wires: # graph 하나씩 빼기
-        s = wire[0]
-        e = wire[1]
-        index1 = graph[s].index(e)
-        index2 = graph[e].index(s)
-        graph[s].pop(index1)
-        graph[e].pop(index2)
-        
-        count = []
-        for i in range(1, n+1):
-            if not visited[i]:
-                value = bfs(i, visited, graph)
-                count.append(value)
-        
-        answer = min(answer, max(count) - min(count))
-        
-        graph[s].append(e)
-        graph[e].append(s)
+    for i in range(len(wires)):
+        graph = [[] for _ in range(n+1)]
         visited = [False] * (n+1)
+        diff = []
         
+        for j in range(len(wires)):
+            if i == j:
+                continue
+            s, e = wires[j][0], wires[j][1]
+            graph[s].append(e)
+            graph[e].append(s)
+        
+        for k in range(1, n+1):
+            if not visited[k]:
+                diff.append(bfs(visited, graph, k))
+                
+        answer = min(answer, abs(diff[0]-diff[1]))        
+
     return answer
