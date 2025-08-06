f49afc9b29f7b40a77d5909a81da1d43f4b80fa1
Gahyeon Lee <100428958+gashine20@users.noreply.github.com>
2025-08-06 22:47:16 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=python3#) 
 
 ### 성능 요약
 
-메모리: 9.35 MB, 시간: 0.02 ms
+메모리: 9.19 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 06일 22:39:55
+2025년 08월 06일 22:47:12
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,25 +1,13 @@
 def solution(n, lost, reserve):
     answer = 0
-    uniform = [1] * (n+1)
+    lost_set = set(lost) - set(reserve)
+    reserve_set = set(reserve) - set(lost)
     
-    for i in lost:
-        uniform[i] -= 1
-    for i in reserve:
-        uniform[i] += 1
-        
-    for i in range(1, n+1):
-        if uniform[i] == 2 and uniform[i-1] == 0:  
-            uniform[i-1]+=1
-            uniform[i] -=1
-        if uniform[i] == 2 and i + 1 <= n and uniform[i+1] == 0:
-            uniform[i+1]+=1
-            uniform[i] -=1
-    
-        # print(uniform)
-    
-    for k in uniform[1:]:
-        if k >= 1:
-            answer += 1
+    for r in reserve_set:
+        if r-1 in lost_set:
+            lost_set.remove(r-1)
+        elif r+1 in lost_set:
+            lost_set.remove(r+1)
     
     
-    return answer
+    return n - len(lost_set)
