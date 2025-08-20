db63f54037de582e06f18fc81213273dd9df0d43
Gahyeon Lee <100428958+gashine20@users.noreply.github.com>
2025-08-20 17:44:51 +0900

    [level 2] Title: 땅따먹기, Time: 344.70 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -4,6 +4,10 @@ def solution(land):
     
     for i in range(1, n):
         for j in range(4):
-            land[i][j] += max(land[i-1][k] for k in range(4) if k != j)
+            land[i][j] = land[i][j] + max([land[i-1][k] for k in range(4) if j != k])
             
     return max(land[-1])
+
+
+# land[1] 기준으로 보면
+# land[1][0] 은 위에서 밟을 수 있는 방법이 land[0][0] 빼고 다 가능하다.
