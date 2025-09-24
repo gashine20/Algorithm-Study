5c7bf95e09f875926e414ade17d8e1da73da2177
Gahyeon Lee <100428958+gashine20@users.noreply.github.com>
2025-09-24 14:25:47 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.55 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -4,38 +4,9 @@ class Solution {
     public int solution(String s) {
         String result = "";
         String[] number = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        List<String> numList = new ArrayList<>(Arrays.asList(number));
-        
-        String S = "";
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if(Character.isDigit(c)){
-                // 문자열 숫자로 바꾸기
-                String k = "";
-                for(int j = 0; j < S.length(); j++){
-                    k += S.charAt(j);
-                    if(numList.contains(k)){
-                        result += numList.indexOf(k);
-                        k = "";
-                    }
-                }
-                result += c;
-                S = "";
-            } else{
-                S += c;
-            }
+        for(int i = 0; i < number.length; i++){
+            s = s.replaceAll(number[i], Integer.toString(i));
         }
-        
-        // S에 있는 값 result에 추가
-        String k = "";
-        for(int j = 0; j < S.length(); j++){
-            k += S.charAt(j);
-            if(numList.contains(k)){
-                result += numList.indexOf(k);
-                k = "";
-            }
-        }
-        
-        return Integer.parseInt(result);
+        return Integer.parseInt(s);
     }
 }
