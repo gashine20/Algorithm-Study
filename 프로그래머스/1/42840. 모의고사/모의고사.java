54bd1318639de9bad47298bd34e2d8ff0e0eda42
Gahyeon Lee <100428958+gashine20@users.noreply.github.com>
2026-01-03 18:08:43 +0900

    [level 1] Title: 모의고사, Time: 3.90 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,37 +2,38 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] supo1 = new int[]{1,2,3,4,5};
-        int[] supo2 = new int[]{2,1,2,3,2,4,2,5};
-        int[] supo3 = new int[]{3,3,1,1,2,2,4,4,5,5};
+        ArrayList<Integer> answerList = new ArrayList<>();
         
-        int[] correctCount = new int[]{0, 0, 0};
+        ArrayList<int[]> supojas = new ArrayList<>();
+        supojas.add(new int[]{1,2,3,4,5});
+        supojas.add(new int[]{2,1,2,3,2,4,2,5});
+        supojas.add(new int[]{3,3,1,1,2,2,4,4,5,5});
         
-        for(int i = 0; i < answers.length; i++){
-            int index1 = i % supo1.length;
-            if(supo1[index1] == answers[i]){correctCount[0]++;}
-            
-            int index2 = i % supo2.length;
-            if(supo2[index2] == answers[i]){correctCount[1]++;}
-            
-            int index3 = i % supo3.length;
-            if(supo3[index3] == answers[i]){correctCount[2]++;}
-        }
-        
-        
-        int[] correctCountCopy = Arrays.copyOf(correctCount, correctCount.length);
-        Arrays.sort(correctCountCopy);
-        int maxCorrect = correctCountCopy[correctCount.length - 1];
-        
-        ArrayList<Integer> answerlist = new ArrayList<>();
-        for(int i = 0; i < 3; i++){
-            if(maxCorrect == correctCount[i]){
-                answerlist.add(i+1);
+        int correctAnswer = 0;
+        int index = 1;
+        for(int[] supoja : supojas){
+            int count = 0;
+            for(int i = 0; i < answers.length; i++ ){
+                if(answers[i] == supoja[i%supoja.length]){
+                    count+=1;
+                }
             }
+            if(correctAnswer < count) {
+                answerList.clear();
+                answerList.add(index);
+                correctAnswer = count;
+            } else if (correctAnswer == count) {
+                answerList.add(index);
+            }
+                
+            index +=1;
         }
         
-        // covert arraylist to array
-        int[] answer = answerlist.stream().mapToInt(Integer::intValue).toArray();
+        // 형 변환
+        int[] answer = answerList.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
+        
         return answer;
     }
 }
