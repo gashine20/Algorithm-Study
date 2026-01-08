55da51fa61d2c66b088621004b31a9ae7627dea2
Gahyeon Lee <100428958+gashine20@users.noreply.github.com>
2026-01-09 00:47:42 +0900

    [level 2] Title: 기능개발, Time: 2.55 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
 
 ### 성능 요약
 
-메모리: 9.15 MB, 시간: 0.03 ms
+메모리: 91.9 MB, 시간: 2.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 23일 18:24:51
+2026년 01월 09일 00:47:37
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,33 +2,37 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        ArrayList<Integer> result = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         
-        int[] time = new int[progresses.length];
-        for(int i = 0 ; i < time.length; i++){
-            time[i] = (100 - progresses[i]) / speeds[i];
-            if((100 - progresses[i]) % speeds[i] != 0){
-                time[i] += 1;
-            }
+        int[] result = new int[progresses.length];
+        
+        for(int i = 0; i < progresses.length; i++) {
+            int remain = 100 - progresses[i];
+            int done = (remain % speeds[i] != 0) ? (remain / speeds[i]) + 1 : (remain / speeds[i]);
+            result[i] = done;
         }
         
-        int compare = time[0];
-        int count = 1;
-        
-        for(int i = 1; i < time.length; i++){
-            if(compare >= time[i]){
-                count += 1;
+        int line = result[0];
+        int count = 0;
+        for(int i = 0; i < result.length; i++){
+            if(line < result[i]) {
+                list.add(count);
+                line = result[i];
+                count = 1; // 초기화
             } else{
-                result.add(count);
-                count = 1;
-                compare = time[i];
+                count += 1;
             }
         }
-        result.add(count); // 마지막에 count 넣음
         
-        int[] answer = result.stream().mapToInt(i -> i).toArray();
+        // 마지막 넣기
+        list.add(count);
         
+        // list -> array
+        int[] answer = list.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
         
+
         return answer;
     }
 }
