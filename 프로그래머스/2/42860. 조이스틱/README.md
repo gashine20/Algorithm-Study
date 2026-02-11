53bb667b9ce0c4576411a2961b52c120d0c7a6be
Gahyeon Lee <100428958+gashine20@users.noreply.github.com>
2026-02-11 17:49:02 +0900

    [level 2] Title: 조이스틱, Time: 0.08 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=java) 
 
 ### 성능 요약
 
-메모리: 9.29 MB, 시간: 0.03 ms
+메모리: 85.8 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 08일 00:22:48
+2026년 02월 11일 17:48:59
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -3,37 +3,63 @@ import java.util.*;
 class Solution {
     public int solution(String name) {
         int answer = 0;
-        int len = name.length();
-        int move = name.length()-1; // 기본 최소 오왼이동
-        for(int i = 0; i < len; i++){
-            // 위/아래 알파벳 최소 변경
-            answer += Math.min(name.charAt(i) - 'A', 'Z'-name.charAt(i)+1);
-            
-            // 'A'가 끝나는 지점 찾기
-            int next = i+1;
-            while(next < len && name.charAt(next) == 'A'){
-                next++;
-            }
-            
-            // 좌우이동 최소 구하기
-            move = Math.min(move, (i*2) + len - next);
-            move = Math.min(move, (len-next)*2 +i);         
+        
+        
+        // 위아래 알파벳 최소 변경
+        for(char c : name.toCharArray()) {
+            int result = alphaMove(c);
+            answer+= result;
         }
         
-        answer += move;
+        
+        // 왼오 커서 위치 최소 변경
+        // 1 ~ 10
+        // 1          7 8 9
+        // 1 -> 9 (2) + 9-> 8 + 8 -> 7 => 4
+        // 1 -> 7 (6) + 7 -> 8 + 8 -> 9 => 8
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < name.length(); i++) {
+            if(name.charAt(i) != 'A'){ // 'A'가 아니면
+                list.add(i); // index값 list에 추가
+            }
+        }
+        
+        int count = positionMove(name);
+        answer += count;
+        
         
         return answer;
     }
+    
+    public int positionMove(String name) {
+        // list = 1 7 8 9 
+        int n = name.length();
+        int move = n -1;
+        
+        for(int i = 0; i < n; i++) {
+            int next = i +1;
+            
+            while(next < n && name.charAt(next)=='A') {
+                next++;
+            }
+            
+            // 오른쪽으로 갔다가 되돌아오기
+            int case1 = i * 2 + (n - next);
+            
+            // 왼쪽 먼저 갔다가 오기
+            int case2 = i + 2 * (n-next);
+            
+            move = Math.min(move, Math.min(case1, case2));
+        }
+        
+        return move;
+    }
+    
+    public int alphaMove(char c) {
+        int A = 'A' - '0';
+        int Z = 'Z' - '0';
+        int C = c - '0';
+        
+        return Math.min(C-A, Z-C+1); // (오른쪽 , 왼쪽)
+    }
 }
-
-//"JAN" 첫번째에서 위9, 왼쪽1, 위13 = 총 23
-// B~N 위 1~13 Z~M 아래 1~13
-// 커서 이동 - 오/왼 갈지
-
-// "JABB" 위9, 왼쪽1, 위1, 왼쪽1, 위1 = 13
-// 또는 위9, 오2 위1 오1 위1 = 14
-
-// 오,왼 커서 이동 따로 - {1,1,0,0,1} 이면 움직임3, 
-// 항상, 1에서 3으로 가야하는 지금, 
-
-// 알파벳 변경 따로 - 고정된값
